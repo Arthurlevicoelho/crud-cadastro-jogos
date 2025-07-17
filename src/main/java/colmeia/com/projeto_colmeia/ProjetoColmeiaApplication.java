1be0e3eb6b297cbee20cbca80dc49823be3501d3
Arthur Coelho <arthurlevicoelho@hotmail.com>
2025-07-17 12:07:22 -0300

    feat: adding main

---
+++ src/main/java/colmeia/com/projeto_colmeia/ProjetoColmeiaApplication.java
@@ -0,0 +1,13 @@
+package colmeia.com.projeto_colmeia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoColmeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoColmeiaApplication.class, args);
+	}
+
+}
