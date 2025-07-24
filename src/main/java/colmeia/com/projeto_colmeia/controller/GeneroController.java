9d0691bcccc6f9cba73cd9f7ee613afa3367cf48
Arthur Coelho <arthurlevicoelho@hotmail.com>
2025-07-24 16:59:17 -0300

    feat: adding methods

--- src/main/java/colmeia/com/projeto_colmeia/controller/GeneroController.java
+++ src/main/java/colmeia/com/projeto_colmeia/controller/GeneroController.java
@@ -1,16 +1,35 @@
 package colmeia.com.projeto_colmeia.controller;
 
+import colmeia.com.projeto_colmeia.model.ResponseModel;
+import colmeia.com.projeto_colmeia.model.entity.DesenvolvedorEntity;
 import colmeia.com.projeto_colmeia.model.entity.GeneroEntity;
 import colmeia.com.projeto_colmeia.service.BaseService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import colmeia.com.projeto_colmeia.service.GeneroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/genero")
 public class GeneroController extends BaseController<GeneroEntity, UUID> {
-    public GeneroController(BaseService<GeneroEntity, UUID> service) {
+
+
+    private final GeneroService service;
+
+    public GeneroController(GeneroService service) {
         super(service);
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseModel> save (@RequestBody @Validated GeneroEntity generoEntity) {
+        return service.save(generoEntity);
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseModel> put (@RequestBody @Validated GeneroEntity generoEntity) {
+        return service.put(generoEntity);
     }
 }
